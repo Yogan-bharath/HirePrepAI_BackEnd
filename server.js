e4f9d3d644c4585057096b9560919d6917ee2b94
Yogan-bharath <yoganbharath@gmail.com>
2026-09-11 00:06:58 +0530

    make change in server.js

--- server.js
+++ server.js
@@ -2,12 +2,8 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const app = require("./src/app.js");
-const DB = require("./src/config/database.js")
+const DB = require("./src/config/database.js");
 
-const PORT = process.env.PORT || 3000
+DB();
 
-
-app.listen(PORT,async ()=>{
-    await DB()
-    console.log(`Server is running on http://localhost:${PORT}`)
-})
+module.exports = app;
